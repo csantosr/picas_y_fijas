a73eaaa043164eb8035daeb8e47ada5574037515
Cristian Fernando Santos Ribero <csantosr@Cristians-MacBook-Pro.local>
2022-08-23 20:46:49 -0500

    Check picas y fijas

--- agent.js
+++ agent.js
@@ -6,6 +6,7 @@ class Agent {
     if (perception === 'S') {
       const shuffled = DIGITS.sort(() => 0.5 - Math.random());
       this.agentNumber = shuffled.slice(0, 4).join('');
+      console.log(this.agentNumber);
       return 'R';
     }
     if (perception === '#') {
@@ -14,7 +15,23 @@ class Agent {
     }
     if (!isNaN(perception)) {
       // Un número natural de cuatro dígitos diferentes (puede empezar con cero) a lo que debe responder con el número de picas y fijas respecto a su número. Se percibe como cadena de caracteres.
-      return '0,0';
+      let cleanPerception = perception;
+      if (perception.length > 4) {
+        console.log('Tramposo');
+      } else if (perception.length < 4) {
+        cleanPerception = perception.padStart(4, '0');
+      }
+      let picas = 0, fijas = 0;
+      for(let i = 0; i < cleanPerception.length; i++) {
+        for(let j = 0; j < this.agentNumber.length; j++) {
+          if (cleanPerception[i] === this.agentNumber[j] && i === j) {
+            fijas++;
+          } else if (cleanPerception[i] === this.agentNumber[j]) {
+            picas++;
+          }
+        }
+      }
+      return `${picas},${fijas}`;
     }
     if (!isNaN(perception[0]) && perception[1] === ',' && !isNaN(perception[2])) {
       if (Number(perception[0]) + Number(perception[2]) > 4) {
--- env.js
+++ env.js
@@ -3,3 +3,10 @@ import Agent from "./agent.js";
 const agent = new Agent();
 
 agent.compute('S');
+console.log(agent.compute('0123'));
+console.log(agent.compute('4567'));
+console.log(agent.compute('4567'));
+console.log(agent.compute('7395'));
+console.log(agent.compute('9125'));
+console.log(agent.compute('5473'));
+console.log(agent.compute('6285'));
